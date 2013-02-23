d018e4eda4d9525731307850afd00cb8336a47c1
Mark Peek <mark@peek.org>
2013-02-23 11:27:41 -0800

    Make setup.py pass pep8

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, Extension, find_packages
 
-setup(name='troposphere',
-      version='0.1.2',
-      description="AWS CloudFormation creation library",
-      author="Mark Peek",
-      author_email="mark@peek.org",
-      url="https://github.com/cloudtools/troposphere",
-      license="New BSD license",
-      packages=['troposphere'],
-      use_2to3=True,
+setup(
+    name='troposphere',
+    version='0.1.2',
+    description="AWS CloudFormation creation library",
+    author="Mark Peek",
+    author_email="mark@peek.org",
+    url="https://github.com/cloudtools/troposphere",
+    license="New BSD license",
+    packages=['troposphere'],
+    use_2to3=True,
 )
