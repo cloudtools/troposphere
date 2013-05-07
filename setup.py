7264aa32ccdad4b750b7937acbbf737251729e62
Michael Barrett <loki77@gmail.com>
2013-05-07 09:57:06 -0700

    Bumping version to 0.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='troposphere',
-    version='0.2.8',
+    version='0.2.9',
     description="AWS CloudFormation creation library",
     author="Mark Peek",
     author_email="mark@peek.org",
