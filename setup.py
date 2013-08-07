ac0aae01785caafbcf4742b1e717910c0a91ded1
Mark Peek <mark@peek.org>
2013-08-07 10:44:45 -0700

    Bumping version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='troposphere',
-    version='0.2.9',
+    version='0.3.0',
     description="AWS CloudFormation creation library",
     author="Mark Peek",
     author_email="mark@peek.org",
--- troposphere/__init__.py
+++ troposphere/__init__.py
@@ -10,7 +10,7 @@ import types
 
 from . import validators
 
-__version__ = "0.2.9"
+__version__ = "0.3.0"
 
 # constants for DeletionPolicy
 Delete = 'Delete'
