e91709698f4cc72968014805e9b5055c6bf7aee7
Ilya Margolin <ilya@ulani.de>
2014-01-01 19:55:01 +0100

    Check output in test_examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,6 +2,8 @@ import os
 import re
 import sys
 import unittest
+import types
+from StringIO import StringIO
 
 from functools import partial
 
@@ -10,17 +12,21 @@ class TestExamples(unittest.TestCase):
     pass
 
 
-def _test_file(filename):
-    # Ignore the output
+def _test_file(filename, expected_output):
+    # capture the output
     saved = sys.stdout
-    with open('/dev/null', 'w') as stdout:
+    stdout = StringIO()
+    try:
         sys.stdout = stdout
-        try:
-            with open(filename) as f:
-                code = compile(f.read(), filename, 'exec')
-                exec(code)
-        finally:
-            sys.stdout = saved
+        with open(filename) as f:
+            code = compile(f.read(), filename, 'exec')
+            exec(code) # in {'__name__': '__main__'}
+    finally:
+        sys.stdout = saved
+    # rewind fake stdout so we can read it
+    stdout.seek(0)
+    actual_output = stdout.read()
+    assert expected_output == actual_output
 
 
 def add_tests():
@@ -32,7 +38,9 @@ def add_tests():
     # Add new test functions to the TestExamples class
     for f in example_filesnames:
         testname = 'test_' + f[:-3]
-        testfunc = partial(_test_file, examples + '/' + f)
+        expected_output = open('tests/examples_output/%s.template' %
+                f[:-3]).read()
+        testfunc = partial(_test_file, examples + '/' + f, expected_output)
         # Get rid of partial() __doc__
         testfunc.__doc__ = None
         setattr(TestExamples, testname, testfunc)
