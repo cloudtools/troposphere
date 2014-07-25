5bc77442a6fce5b18054e517f32f33e8ffb6c8d4
Malthe Borch <mborch@gmail.com>
2014-07-25 11:55:51 +0200

    Add support for 'VersioningConfiguration' on S3 buckets.

---
+++ examples/S3_Bucket_With_Versioning.py
@@ -0,0 +1,31 @@
+# Converted from S3_Bucket.template located at:
+# http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
+
+from troposphere import Output, Ref, Template
+from troposphere.s3 import Bucket, PublicRead, VersioningConfiguration
+
+
+t = Template()
+
+t.add_description(
+    "AWS CloudFormation Sample Template S3_Bucket: Sample template showing "
+    "how to create a publicly accessible S3 bucket. "
+    "**WARNING** This template creates an Amazon EC2 instance. "
+    "You will be billed for the AWS resources used if you create "
+    "a stack from this template.")
+
+s3bucket = t.add_resource(Bucket(
+    "S3Bucket",
+    AccessControl=PublicRead,
+    VersioningConfiguration=VersioningConfiguration(
+        Status="Enabled",
+    )
+))
+
+t.add_output(Output(
+    "BucketName",
+    Value=Ref(s3bucket),
+    Description="Name of S3 bucket to hold website content"
+))
+
+print(t.to_json())
