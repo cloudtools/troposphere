58c32db008f2c7a9f3c3ef481fa8408433169797
philtay <philtay51@hotmail.com>
2014-09-03 00:29:32 +0200

    Define several useful constants

---
+++ troposphere/constants.py
@@ -0,0 +1,64 @@
+#
+# Pseudo Parameters
+#
+
+AWS_ACCOUNT_ID = 'AWS::AccountId'
+AWS_NOTIFICATION_ARNS = 'AWS::NotificationARNs'
+AWS_NO_VALUE = 'AWS::NoValue'
+AWS_REGION = 'AWS::Region'
+AWS_STACK_ID = 'AWS::StackId'
+AWS_STACK_NAME = 'AWS::StackName'
+
+#
+# Regions
+#
+
+AP_NORTHEAST_1 = 'ap-northeast-1'
+AP_SOUTHEAST_1 = 'ap-southeast-1'
+AP_SOUTHEAST_2 = 'ap-southeast-2'
+EU_WEST_1 = 'eu-west-1'
+SA_EAST_1 = 'sa-east-1'
+US_EAST_1 = 'us-east-1'
+US_WEST_1 = 'us-west-1'
+US_WEST_2 = 'us-west-2'
+
+#
+# Availability Zones
+#
+
+AP_NORTHEAST_1A = 'ap-northeast-1a'
+AP_NORTHEAST_1B = 'ap-northeast-1b'
+AP_NORTHEAST_1C = 'ap-northeast-1c'
+
+AP_SOUTHEAST_1A = 'ap-southeast-1a'
+AP_SOUTHEAST_1B = 'ap-southeast-1b'
+
+AP_SOUTHEAST_2A = 'ap-southeast-2a'
+AP_SOUTHEAST_2B = 'ap-southeast-2b'
+
+EU_WEST_1A = 'eu-west-1a'
+EU_WEST_1B = 'eu-west-1b'
+EU_WEST_1C = 'eu-west-1c'
+
+SA_EAST_1A = 'sa-east-1a'
+SA_EAST_1B = 'sa-east-1b'
+
+US_EAST_1A = 'us-east-1a'
+US_EAST_1B = 'us-east-1b'
+US_EAST_1C = 'us-east-1c'
+US_EAST_1D = 'us-east-1d'
+US_EAST_1E = 'us-east-1e'
+
+US_WEST_1A = 'us-west-1a'
+US_WEST_1B = 'us-west-1b'
+US_WEST_1C = 'us-west-1c'
+
+US_WEST_2A = 'us-west-2a'
+US_WEST_2B = 'us-west-2b'
+US_WEST_2C = 'us-west-2c'
+
+#
+# Networking
+#
+
+QUAD_ZERO = '0.0.0.0/0'
