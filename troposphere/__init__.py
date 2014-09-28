c47a7abd5374fe6ec67dc586df5ae05a6ee06042
Mark Peek <mark@peek.org>
2014-09-28 09:32:20 -0700

    Move pseudo parameters from constants to the base troposphere namespace

--- troposphere/__init__.py
+++ troposphere/__init__.py
@@ -17,6 +17,14 @@ Delete = 'Delete'
 Retain = 'Retain'
 Snapshot = 'Snapshot'
 
+# Pseudo Parameters
+AWS_ACCOUNT_ID = 'AWS::AccountId'
+AWS_NOTIFICATION_ARNS = 'AWS::NotificationARNs'
+AWS_NO_VALUE = 'AWS::NoValue'
+AWS_REGION = 'AWS::Region'
+AWS_STACK_ID = 'AWS::StackId'
+AWS_STACK_NAME = 'AWS::StackName'
+
 valid_names = re.compile(r'^[a-zA-Z0-9]+$')
 
 
--- troposphere/constants.py
+++ troposphere/constants.py
@@ -1,15 +1,4 @@
 #
-# Pseudo Parameters
-#
-
-AWS_ACCOUNT_ID = 'AWS::AccountId'
-AWS_NOTIFICATION_ARNS = 'AWS::NotificationARNs'
-AWS_NO_VALUE = 'AWS::NoValue'
-AWS_REGION = 'AWS::Region'
-AWS_STACK_ID = 'AWS::StackId'
-AWS_STACK_NAME = 'AWS::StackName'
-
-#
 # Regions
 #
 
