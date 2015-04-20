b6a1890508b0fb031cf91935260544a08080fe42
philtay <philtay51@hotmail.com>
2015-04-20 17:09:31 +0200

    Fix Ref import

--- troposphere/rds.py
+++ troposphere/rds.py
@@ -3,7 +3,7 @@
 #
 # See LICENSE file for full license.
 
-from . import AWSHelperFn, AWSObject, AWSProperty
+from . import AWSHelperFn, AWSObject, AWSProperty, Ref
 from .validators import boolean, network_port, positive_integer
 
 
