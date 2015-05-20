5744ffb69ea90859671050de41aa69830d8cfe45
Hugo Lopes Tavares <hltbra@gmail.com>
2015-05-20 18:13:55 -0400

    Address pep8 issues

--- tests/test_int_type.py
+++ tests/test_int_type.py
@@ -7,10 +7,11 @@ from troposphere import AWSObject
 
 class TestIntTypeShouldNotBeUsed(unittest.TestCase):
 
-    def test_there_should_not_be_any_awsobject_that_uses_int_in_their_fields(self):
+    def test_there_should_not_be_any_awsobject_with_int_in_props(self):
         """
-        Anything that requires an integer should use `validators.integer` rather than `int`
-        `int` does not work with types that behave like an integer (i.e. `long` in Python 2)
+        Anything that requires an integer should use `validators.integer`
+        rather than `int`, because `int` does not work with types
+        that behave like an integer (i.e. `long` in Python 2).
         """
         aws_objects = self.get_aws_objects()
         for obj in aws_objects:
@@ -22,13 +23,19 @@ class TestIntTypeShouldNotBeUsed(unittest.TestCase):
         for module in modules:
             for name in dir(module):
                 obj = getattr(module, name)
-                if isinstance(obj, type) and obj != AWSObject and issubclass(obj, AWSObject):
+                if (
+                        isinstance(obj, type) and
+                        obj != AWSObject and
+                        issubclass(obj, AWSObject)
+                ):
                     result.append(obj)
         return result
 
     def assertNoIntType(self, obj):
         for prop_name, (types, required) in obj.props.items():
-            error_msg = "{}.props['{}'] should have `validators.integer` rather than `int`".format(obj.__name__, prop_name)
+            error_msg = (
+                "{}.props['{}'] should have `validators.integer` "
+                "rather than `int`".format(obj.__name__, prop_name))
             if isinstance(types, collections.Iterable):
                 assert int not in types, error_msg
             else:
@@ -36,9 +43,9 @@ class TestIntTypeShouldNotBeUsed(unittest.TestCase):
 
     def _get_all_troposphere_modules(self, dirname):
         return [
-            package_name
-            for importer, package_name, is_pkg in pkgutil.walk_packages(dirname)
-                if not is_pkg and package_name.startswith('troposphere')
+            pkg_name
+            for importer, pkg_name, is_pkg in pkgutil.walk_packages(dirname)
+            if not is_pkg and pkg_name.startswith('troposphere')
         ]
 
     def _import_all_modules(self):
