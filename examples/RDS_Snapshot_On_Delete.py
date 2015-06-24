87c1f07632009e8cc37d3746c176133af9c62ba8
George Yoshida <dynkin@gmail.com>
2015-06-24 16:59:41 +0900

    Add RDS_Snapshot_On_Delete.template example
    - add test sample

--- examples/RDS_Snapshot_On_Delete.py
+++ examples/RDS_Snapshot_On_Delete.py
@@ -10,8 +10,13 @@ t = Template()
 
 t.add_version("2010-09-09")
 
-t.add_description("""\
-AWS CloudFormation Sample Template RDS_Snapshot_On_Delete: Sample template showing how to create an RDS DBInstance that is snapshotted on stack deletion. **WARNING** This template creates an Amazon RDS database instance. When the stack is deleted a database snpshot will be left in your account. You will be billed for the AWS resources used if you create a stack from this template.""")
+t.add_description(
+    "AWS CloudFormation Sample Template RDS_Snapshot_On_Delete: Sample "
+    "template showing how to create an RDS DBInstance that is snapshotted on "
+    "stack deletion. **WARNING** This template creates an Amazon RDS database "
+    "instance. When the stack is deleted a database snpshot will be left in "
+    "your account. You will be billed for the AWS resources used if you "
+    "create a stack from this template.")
 MyDB = t.add_resource(DBInstance(
     "MyDB",
     Engine="MySQL",
@@ -25,7 +30,13 @@ MyDB = t.add_resource(DBInstance(
 JDBCConnectionString = t.add_output(Output(
     "JDBCConnectionString",
     Description="JDBC connection string for the database",
-    Value=Join("", ["jdbc:mysql://", GetAtt(MyDB, "Endpoint.Address"), ":", GetAtt(MyDB, "Endpoint.Port"), "/MyDatabase"]),
+    Value=Join("", [
+        "jdbc:mysql://",
+        GetAtt(MyDB, "Endpoint.Address"),
+        ":",
+        GetAtt(MyDB, "Endpoint.Port"),
+        "/MyDatabase"
+    ]),
 ))
 
 print(t.to_json())
