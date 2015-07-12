3ccf7b7d889235473558a1b49c279164624f817e
Mark Peek <mark@peek.org>
2015-07-12 13:55:03 -0700

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='troposphere',
-    version='1.1.0',
+    version='1.1.1',
     description="AWS CloudFormation creation library",
     author="Mark Peek",
     author_email="mark@peek.org",
