62f931421556dd98d7509829dbabdf36c4773d51
Michael Barrett <loki77@gmail.com>
2015-08-22 16:58:17 -0700

    Add more specific validators to DBInstance
    This matches all the new gotchas in:
    
    http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-rds-database-instance.html#cfn-rds-dbinstance-storagetype

--- tests/test_rds.py
+++ tests/test_rds.py
@@ -7,7 +7,7 @@ class TestRDS(unittest.TestCase):
     def test_it_allows_an_rds_instance_created_from_a_snapshot(self):
         rds_instance = rds.DBInstance(
             'SomeTitle',
-            AllocatedStorage=1,
+            AllocatedStorage=100,
             DBInstanceClass='db.m1.small',
             Engine='MySQL',
             DBSnapshotIdentifier='SomeSnapshotIdentifier'
@@ -64,7 +64,7 @@ class TestRDS(unittest.TestCase):
             DBName="SomeName",
             MasterUsername="SomeUsername",
             MasterUserPassword="SomePassword",
-            PreferredBackupWindow="SomeBackupWindow",
+            PreferredBackupWindow="10:00-11:00",
             MultiAZ=True,
             DBSnapshotIdentifier="SomeDBSnapshotIdentifier",
             DBSubnetGroupName="SomeDBSubnetGroupName",
@@ -101,10 +101,10 @@ class TestRDS(unittest.TestCase):
 
     def test_it_allows_an_rds_instance_with_iops(self):
         # ensure troposphere works with longs and ints
-        long_number = 2 ** 100
+        long_number = long(2000)
         rds_instance = rds.DBInstance(
             'SomeTitle',
-            AllocatedStorage=1,
+            AllocatedStorage=200,
             DBInstanceClass='db.m1.small',
             Engine='MySQL',
             MasterUsername='SomeUsername',
@@ -133,3 +133,115 @@ class TestRDS(unittest.TestCase):
             ]
         )
         rds_optiongroup.JSONrepr()
+
+    def test_fail_az_and_multiaz(self):
+        i = rds.DBInstance(
+            "NoAZAndMultiAZ",
+            MasterUsername="myuser",
+            MasterUserPassword="mypassword",
+            AllocatedStorage=10,
+            DBInstanceClass="db.m1.small",
+            Engine="postgres",
+            AvailabilityZone="us-east-1",
+            MultiAZ=True)
+        with self.assertRaisesRegexp(ValueError, "if MultiAZ is set to "):
+            i.JSONrepr()
+
+    def test_io1_storage_type_and_iops(self):
+        i = rds.DBInstance(
+            "NoAZAndMultiAZ",
+            MasterUsername="myuser",
+            MasterUserPassword="mypassword",
+            AllocatedStorage=10,
+            DBInstanceClass="db.m1.small",
+            Engine="postgres",
+            StorageType='io1')
+        with self.assertRaisesRegexp(ValueError,
+                                     "Must specify Iops if "):
+            i.JSONrepr()
+
+    def test_storage_to_iops_ratio(self):
+        i = rds.DBInstance(
+            "NoAZAndMultiAZ",
+            MasterUsername="myuser",
+            MasterUserPassword="mypassword",
+            DBInstanceClass="db.m1.small",
+            Engine="postgres",
+            StorageType='io1',
+            Iops=4000,
+            AllocatedStorage=10)
+        with self.assertRaisesRegexp(ValueError,
+                                     " must be at least 100 "):
+            i.JSONrepr()
+
+        i.AllocatedStorage = 100
+        with self.assertRaisesRegexp(ValueError,
+                                     " must be no less than 1/10th "):
+            i.JSONrepr()
+
+        i.AllocatedStorage = 400
+        i.JSONrepr()
+
+
+class TestRDSValidators(unittest.TestCase):
+    def test_validate_iops(self):
+        with self.assertRaises(ValueError):
+            rds.validate_iops(500)
+        with self.assertRaises(ValueError):
+            rds.validate_iops(20000)
+        rds.validate_iops(2000)
+
+    def test_validate_storage_type(self):
+        for t in rds.VALID_STORAGE_TYPES:
+            rds.validate_storage_type(t)
+
+        with self.assertRaises(ValueError):
+            rds.validate_storage_type("bad_storage_type")
+
+    def test_validate_engine(self):
+        for e in rds.VALID_DB_ENGINES:
+            rds.validate_engine(e)
+
+        with self.assertRaises(ValueError):
+            rds.validate_engine("bad_engine")
+
+    def test_validate_license_model(self):
+        for lm in rds.VALID_LICENSE_MODELS:
+            rds.validate_license_model(lm)
+
+        with self.assertRaises(ValueError):
+            rds.validate_license_model("bad_license_model")
+
+    def test_validate_backup_window(self):
+        good_windows = ("10:00-11:00", "22:00-06:00")
+        for w in good_windows:
+            rds.validate_backup_window(w)
+
+        bad_format = ("bad_backup_window", "28:11-10:00", "10:00-28:11")
+        for w in bad_format:
+            with self.assertRaisesRegexp(ValueError, "must be in the format"):
+                rds.validate_backup_window(w)
+
+        with self.assertRaisesRegexp(ValueError, "must be at least 30 "):
+            rds.validate_backup_window("10:00-10:10")
+
+    def test_validate_maintenance_window(self):
+        good_windows = ("Mon:10:00-Mon:16:30", "Mon:10:00-Wed:10:00",
+                        "Sun:16:00-Mon:11:00")
+
+        for w in good_windows:
+            rds.validate_maintenance_window(w)
+
+        bad_format = ("bad_mainteance", "Mon:10:00-Tue:28:00", "10:00-22:00")
+        for w in bad_format:
+            with self.assertRaisesRegexp(ValueError, "must be in the format"):
+                rds.validate_maintenance_window(w)
+
+        bad_days = ("Boo:10:00-Woo:10:30", "Boo:10:00-Tue:10:30",
+                    "Mon:10:00-Boo:10:30")
+        for w in bad_days:
+            with self.assertRaisesRegexp(ValueError, " day part of ranges "):
+                rds.validate_maintenance_window(w)
+
+        with self.assertRaisesRegexp(ValueError, "must be at least 30 "):
+            rds.validate_maintenance_window("Mon:10:00-Mon:10:10")
