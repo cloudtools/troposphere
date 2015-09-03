642716c0d16a9c32c50349a647d75a004c556cf2
Craig Dodd <craig@dodd.io>
2015-09-03 14:57:56 +0100

    Add AWS::EC2::VPCEndpoint support
    Add support for VPC endpoints, as per http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ec2-vpcendpoint.html

--- troposphere/ec2.py
+++ troposphere/ec2.py
@@ -402,6 +402,16 @@ class VPNConnectionRoute(AWSObject):
     }
 
 
+class VPCEndpoint(AWSObject):
+    resource_type = "AWS::EC2::VPCEndpoint"
+
+    props = {
+        'RouteTableIds': (list, True),
+        'ServiceName': (basestring, True),
+        'VpcId': (basestring, True),
+    }
+
+
 class VPNGateway(AWSObject):
     resource_type = "AWS::EC2::VPNGateway"
 
