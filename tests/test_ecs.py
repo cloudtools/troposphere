16f13ceec3494ad5906c787440013a8ab74e1394
tom <thomas.o.marks@gmail.com>
2015-10-18 22:53:37 +0100

    Add some container definition properties

--- tests/test_ecs.py
+++ tests/test_ecs.py
@@ -51,3 +51,38 @@ class TestECS(unittest.TestCase):
         )
 
         ecs_service.JSONrepr()
+
+    def test_all_container_definition_options(self):
+        container_def = ecs.ContainerDefinition(
+            Command=['option1', 'option2'],
+            Cpu="100",
+            DisableNetworking=False,
+            DnsSearchDomains=["domain1", "domain2"],
+            DnsServers=["server1", "server2"],
+            DockerSecurityOptions=["option1", "option2"],
+            EntryPoint=["run", "this"],
+            Environment=[ecs.Environment(Name="Env", Value="var")],
+            ExtraHosts=[ecs.ExtraHost(Hostname="Foo", IpAddress="127.0.0.1")],
+            Essential=True,
+            Hostname="myHost",
+            Image="myimage",
+            Links=["name:name"],
+            Memory="300",
+            MountPoints=[ecs.MountPoint(
+                ContainerPath="/path", SourceVolume="Volume", ReadOnly=True)
+            ],
+            Name="mycontainer",
+            PortMappings=[ecs.PortMapping(
+                ContainerPort=2000, HostPort=3000, Protocol="udp")
+            ],
+            Privileged=True,
+            ReadonlyRootFilesystem=True,
+            User="user",
+            ULimits=[ecs.ULimit(Name="core", SoftLimit=1, HardLimit=2)],
+            VolumesFrom=[
+                ecs.VolumesFrom(SourceContainer="Container", ReadOnly=True)
+            ],
+            Workingdirectory="/path/"
+        )
+
+        container_def.JSONrepr()
--- troposphere/ecs.py
+++ troposphere/ecs.py
@@ -1,5 +1,7 @@
 from . import AWSObject, AWSProperty, Ref
-from .validators import boolean, network_port, positive_integer
+from .validators import (
+    boolean, network_port, positive_integer, protocol, ulimit, integer
+)
 
 
 class Cluster(AWSObject):
@@ -47,6 +49,7 @@ class PortMapping(AWSProperty):
     props = {
         'ContainerPort': (network_port, True),
         'HostPort': (network_port, False),
+        'Protocol': (protocol, False)
     }
 
 
@@ -57,20 +60,46 @@ class VolumesFrom(AWSProperty):
     }
 
 
+class ExtraHost(AWSProperty):
+    props = {
+        'Hostname': (basestring, True),
+        'IpAddress': (basestring, True),
+    }
+
+
+class ULimit(AWSProperty):
+    props = {
+        "Name": (ulimit, True),
+        "SoftLimit": (integer, True),
+        "HardLimit": (integer, True),
+    }
+
+
 class ContainerDefinition(AWSProperty):
     props = {
         'Command': ([basestring], False),
         'Cpu': (positive_integer, False),
+        'DisableNetworking': (boolean, False),
+        'DnsSearchDomains': ([basestring], False),
+        'DnsServers': ([basestring], False),
+        'DockerSecurityOptions': ([basestring], False),
         'EntryPoint': ([basestring], False),
         'Environment': ([Environment], False),
+        'ExtraHosts': ([ExtraHost], False),
         'Essential': (boolean, False),
+        'Hostname': (basestring, False),
         'Image': (basestring, True),
         'Links': ([basestring], False),
         'Memory': (positive_integer, True),
         'MountPoints': ([MountPoint], False),
         'Name': (basestring, True),
         'PortMappings': ([PortMapping], False),
+        'Privileged': (boolean, False),
+        'ReadonlyRootFilesystem': (boolean, False),
+        'User': (basestring, False),
+        'ULimits': ([ULimit], False),
         'VolumesFrom': ([VolumesFrom], False),
+        'Workingdirectory': (basestring, False),
     }
 
 
