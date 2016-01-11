2479a613be45a1e7b200c4d7425668656f013393
Sean Rankine <sean.rankine@cloudreach.co.uk>
2016-01-11 16:30:30 +0000

    tidied python files to pass the pep8

--- troposphere/helpers/userdata.py
+++ troposphere/helpers/userdata.py
@@ -1,38 +1,40 @@
+#!/usr/bin/python
+
 import os
 from troposphere import Base64, Join
 
-# helper function to import and format existing file as userdata
-# filepath must be an absolute filepath
 
 def from_file(filepath):
     """Imports userdata from a file.
-    
+
     This function ignore blank lines within the file.
     Special characters are automatically escaped.
 
     Args:
-        filepath (string): The absolute path to the file containing the userdata to be imported.
+        filepath (string): The absolute path to the file
+        containing the userdata to be imported.
 
     Returns:
-        Base64 object: The Base64 object being passed a Join object with strings.
+        Base64 object: The Base64 object being passed a Join object
+        with strings.
         If file not found, an empty string list is used.
 
     """
-    
+
     data = []
-    
+
     try:
-        f = open(filepath, "r")
+        f = open(filepath, 'r')
         try:
             for line in f:
                 if line.strip():
                     data.append(line)
         except:
-            print "Error: Could not parse userdata file - No userdata imported."
+            print 'Error: Could not parse userdata file.'
             data = []
-        
     except IOError:
-        print "Error: Userdata file does not appear to exist. - No userdata imported."
 
+        print 'Error: Userdata file does not appear to exist.'
     finally:
-        return Base64(Join(",", data))
+
+        return Base64(Join(',', data))
