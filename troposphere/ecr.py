86998efa9b76e025d0727ef68cf13f4fff2af197
philtay <philtay51@hotmail.com>
2016-02-26 23:04:11 +0100

    Add Repository resource

---
+++ troposphere/ecr.py
@@ -0,0 +1,14 @@
+from . import AWSObject
+try:
+    from awacs.aws import Policy
+    policytypes = (dict, Policy)
+except ImportError:
+    policytypes = dict,
+
+class Repository(AWSObject):
+    resource_type = "AWS::ECR::Repository"
+
+    props = {
+        'RepositoryName': (basestring, False),
+        'RepositoryPolicyText': (policytypes, False),
+    }
