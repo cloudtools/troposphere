d6ac3a3f790d3a6e2600fcd2e258f578af97604e
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2016-03-03 09:36:46 +0000

    Initial files for EMR support

---
+++ tests/test_emr.py
@@ -0,0 +1,6 @@
+import unittest
+from troposphere import Ref
+import troposphere.emr as emr
+
+class TestEMR(unittest.TestCase):
+    pass
---
+++ troposphere/emr.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2012-2013, Antonio Alonso Dominguez <alonso.domin@gmail.com>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from . import AWSObject, AWSProperty, AWSHelperFn
+from .validators import (boolean, integer)
+
+class KeyValue(AWSHelperFn):
+    def __init__(self, key, value):
+        self.data = { 'Key': key, 'Value': value }
+
+    def JSONrepr(self):
+        return self.data
+
+class Application(AWSProperty):
+    props = {
+        'AdditionalInfo' : (dict, False),
+        'Args'           : (list, False),
+        'Name'           : (basestring, False),
+        'Version'        : (basestring, False)
+    }
+
+class BootstrapActionConfig(AWSProperty):
+    props = {
+        'Name'                  : (basestring, True),
+        'ScriptBootstrapAction' : (basestring, True)
+    }
+
+class ClusterConfiguration(AWSProperty):
+    props = {
+        'Classification'          : (basestring, False),
+        'ConfigurationProperties' : (dict, False),
+        'Configurations'          : (list, False)
+    }
+
+class InstanceGroupConfigProperty(AWSProperty):
+    props = {
+        'BidPrice'       : (basestring, False),
+        'Configurations' : (list, False),
+        'InstanceCount'  : (integer, True),
+        'InstanceType'   : (basestring, True),
+        'Market'         : (basestring, False),
+        'Name'           : (basestring, False)
+    }
+
+class PlacementType(AWSProperty):
+    props = {
+        'AvailabilityZone' : (basestring, True)
+    }
+
+class JobFlowInstancesConfig(AWSProperty):
+    props = {
+        'AdditionalMasterSecurityGroups' : (list, False),
+        'AdditionalSlaveSecurityGroups'  : (list, False),
+        'CoreInstanceGroup'              : (InstanceGroupConfigProperty, True),
+        'Ec2KeyName'                     : (basestring, False),
+        'Ec2SubnetId'                    : (basestring, False),
+        'EmrManagedMasterSecurityGroup'  : (basestring, False),
+        'EmrManagedSlaveSecurityGroup'   : (basestring, False),
+        'HadoopVersion'                  : (basestring, False),
+        'MasterInstanceGroup'            : (InstanceGroupConfigProperty, True),
+        'Placement'                      : (PlacementType, False),
+        'ServiceAccessSecurityGroup'     : (basestring, False),
+        'TerminationProtected'           : (boolean, False)
+    }
+
+class Cluster(AWSObject):
+    resource_type = "AWS::EMR::Cluster"
+
+    props = {
+        'AdditionalInfo'    : (dict, False),
+        'Applications'      : ([Application], False),
+        'BoostrapActions'   : ([BootstrapActionConfig], False),
+        'Configurations'    : ([ClusterConfiguration], False),
+        'Instances'         : (JobFlowInstancesConfig, True),
+        'JobFlowRole'       : (basestring, True),
+        'LogUri'            : (basestring, False),
+        'Name'              : (basestring, True),
+        'ReleaseLabel'      : (basestring, False),
+        'ServiceRole'       : (basestring, True),
+        'Tags'              : (list, False),
+        'VisibleToAllUsers' : (boolean, False)
+    }
+
+class InstanceGroupConfig(AWSObject):
+    resource_type = "AWS::EMR::InstanceGroupConfig"
+
+    props = {
+        'BidPrice'       : (basestring, False),
+        'Configurations' : (list, False),
+        'InstanceCount'  : (integer, True),
+        'InstanceRole'   : (basestring, True),
+        'InstanceType'   : (basestring, True),
+        'Market'         : (basestring, False),
+        'Name'           : (basestring, False)
+    }
+
+class HadoopJarStepConfig(AWSProperty):
+    props = {
+        'Args'      : (basestring, False),
+        'Jar'       : (basestring, True),
+        'MainClass' : (basestring, False)
+    }
+
+class Step(AWSObject):
+    resource_type = "AWS::EMR::Step"
+
+    props = {
+        'ActionOnFailure' : (basestring, True),
+        'HadoopJarStep'   : (HadoopJarStepConfig, True),
+        'JobFlowId'       : (basestring, True),
+        'Name'            : (basestring, True),
+        'StepProperties'  : (list, False)
+    }
