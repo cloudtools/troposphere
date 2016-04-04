4f0a3edf230c6c7c6b3341c42c79e1c2fa890abe
A. Alonso Dominguez <alonso.domin@gmail.com>
2016-04-04 16:27:20 +0100

    Adding support for EMR resources
    * Initial files for EMR support
    
    * Adding some tests for EMR types
    
    * Reformatting the file
    
    * Adding a basic sample of an EMR Cluster plus the expected template
    
    * Fixing formatting
    
    * Fixing print syntax
    
    * Adding model for ScriptBootstrapActions
    
    * Adding configuration properties to the cluster
    
    * Using helper function for colon-separated key-value pair lists.
    
    * Fixing formatting
    
    * Setting a type for the configuration list in instance group resource
    
    * Added additional validators and aligned fixed test template
    
    * Added a Test step into the test template.
    
    * Initial files for EMR support
    
    * Adding some tests for EMR types
    
    * Reformatting the file
    
    * Adding a basic sample of an EMR Cluster plus the expected template
    
    * Fixing formatting
    
    * Fixing print syntax
    
    * Adding model for ScriptBootstrapActions
    
    * Adding configuration properties to the cluster
    
    * Using helper function for colon-separated key-value pair lists.
    
    * Fixing formatting
    
    * Setting a type for the configuration list in instance group resource
    
    * Added additional validators and aligned fixed test template
    
    * Added a Test step into the test template.
    
    * Update validators for security groups and instance count
    
    * Move StepProperties to the appropiate type
    
    * Fix the type for Application args and add step properties to the test template

---
+++ examples/EMR_Cluster.py
@@ -0,0 +1,170 @@
+from troposphere import Parameter, Ref, Template, Tags
+import troposphere.iam as iam
+import troposphere.emr as emr
+
+template = Template()
+template.add_description(
+    "Sample CloudFormation template for creating an EMR cluster"
+)
+
+keyname = template.add_parameter(Parameter(
+    "KeyName",
+    Description="Name of an existing EC2 KeyPair to enable SSH "
+                "to the instances",
+    Type="AWS::EC2::KeyPair::KeyName"
+))
+
+subnet = template.add_parameter(Parameter(
+    "Subnet",
+    Description="Subnet ID for creating the EMR cluster",
+    Type="AWS::EC2::Subnet::Id"
+))
+
+service_access_sg = template.add_parameter(Parameter(
+    "ServiceAccessSecurityGroup",
+    Description="Security Group providing service access to EMR",
+    Type="AWS::EC2::SecurityGroup::Id"
+))
+
+managed_master_sg = template.add_parameter(Parameter(
+    "ManagedMasterSecurityGroup",
+    Description="Security Group (managed by EMR) for master instances",
+    Type="AWS::EC2::SecurityGroup::Id"
+))
+
+managed_slave_sq = template.add_parameter(Parameter(
+    "ManagedSlaveSecurityGroup",
+    Description="Security Group (managed by EMR) for slave instances",
+    Type="AWS::EC2::SecurityGroup::Id"
+))
+
+# IAM roles required by EMR
+
+emr_service_role = template.add_resource(iam.Role(
+    'EMRServiceRole',
+    AssumeRolePolicyDocument={
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "elasticmapreduce.amazonaws.com"
+                ]
+            },
+            "Action": ["sts:AssumeRole"]
+        }]
+    },
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole'
+    ]
+))
+
+emr_job_flow_role = template.add_resource(iam.Role(
+    "EMRJobFlowRole",
+    AssumeRolePolicyDocument={
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            },
+            "Action": ["sts:AssumeRole"]
+        }]
+    },
+    ManagedPolicyArns=[
+        'arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role'
+    ]
+))
+
+emr_instance_profile = template.add_resource(iam.InstanceProfile(
+    "EMRInstanceProfile",
+    Roles=[Ref(emr_job_flow_role)]
+))
+
+# EMR Cluster Resource
+
+cluster = template.add_resource(emr.Cluster(
+    "EMRSampleCluster",
+    Name="EMR Sample Cluster",
+    ReleaseLabel='emr-4.3.0',
+    BootstrapActions=[emr.BootstrapActionConfig(
+        Name='Dummy bootstrap action',
+        ScriptBootstrapAction=emr.ScriptBootstrapActionConfig(
+            Path='/bin/sh',
+            Args=['echo', 'Hello World']
+        )
+    )],
+    Configurations=[
+        emr.Configuration(
+            Classification="core-site",
+            ConfigurationProperties={
+                'hadoop.security.groups.cache.secs': '250'
+            }
+        ),
+        emr.Configuration(
+            Classification="mapred-site",
+            ConfigurationProperties={
+                'mapred.tasktracker.map.tasks.maximum': '2',
+                'mapreduce.map.sort.spill.percent': '90',
+                'mapreduce.tasktracker.reduce.tasks.maximum': '5'
+            }
+        ),
+        emr.Configuration(
+            Classification="hadoop-env",
+            Configurations=[
+                emr.Configuration(
+                    Classification="export",
+                    ConfigurationProperties={
+                        "HADOOP_DATANODE_HEAPSIZE": "2048",
+                        "HADOOP_NAMENODE_OPTS": "-XX:GCTimeRatio=19"
+                    }
+                )
+            ]
+        )
+    ],
+    JobFlowRole=Ref(emr_instance_profile),
+    ServiceRole=Ref(emr_service_role),
+    Instances=emr.JobFlowInstancesConfig(
+        MasterInstanceGroup=emr.InstanceGroupConfigProperty(
+            Name="Master Instance",
+            InstanceCount="1",
+            InstanceType="m3.xlarge",
+            Market="ON_DEMAND"
+        ),
+        CoreInstanceGroup=emr.InstanceGroupConfigProperty(
+            Name="Core Instance",
+            BidPrice="20",
+            InstanceCount="1",
+            InstanceType="m3.xlarge",
+            Market="SPOT"
+        )
+    ),
+    Applications=[
+        emr.Application(Name="Hadoop"),
+        emr.Application(Name="Hive"),
+        emr.Application(Name="Mahout"),
+        emr.Application(Name="Pig"),
+        emr.Application(Name="Spark")
+    ],
+    VisibleToAllUsers="true",
+    Tags=Tags(
+        Name="EMR Sample Cluster"
+    )
+))
+
+step = template.add_resource(emr.Step(
+    'TestStep',
+    Name="TestStep",
+    ActionOnFailure='CONTINUE',
+    HadoopJarStep=emr.HadoopJarStepConfig(
+        Args=["5", "10"],
+        Jar="s3://emr-cfn-test/hadoop-mapreduce-examples-2.6.0.jar",
+        MainClass="pi",
+        StepProperties=[
+            emr.KeyValue('my.custom.property', 'my.custom.value')
+        ]
+    ),
+    JobFlowId=Ref(cluster)
+))
+
+print(template.to_json())
