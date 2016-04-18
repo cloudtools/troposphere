1410d953eb2dcb4b4bd89fd767670bfeac09b848
Sander van de Graaf <mail@svdgraaf.nl>
2016-04-18 23:08:08 +0200

    added api gateway and lambda function

--- examples/ApiGateway.py
+++ examples/ApiGateway.py
@@ -1,5 +1,9 @@
 from troposphere import Parameter, Ref, Template
-from troposphere.apigateway import RestApi
+from troposphere.apigateway import RestApi, Method, Integration, Deployment, Model
+from troposphere.iam import Role, Policy
+from troposphere.awslambda import Function, Code
+from troposphere import FindInMap, GetAtt, Join, Output
+
 
 
 t = Template()
@@ -73,9 +77,74 @@ swagger = """{
   }
 }"""
 
-t.add_resource(RestApi(
+# Create the Api Gateway
+rest_api = t.add_resource(RestApi(
     "ExampleApi",
     Body=swagger,
 ))
+
+model = t.add_resource(Model(
+    "CatModel",
+    Name="Cat",
+    ContentType="application/json",
+    RestApiId=Ref(rest_api)
+))
+
+
+# Create a Lambda function that will be mapped
+code = [
+    "var response = require('cfn-response');",
+    "exports.handler = function(event, context) {",
+    "   return 'foobar!';",
+    "};",
+]
+
+# create a role for the lambda function
+t.add_resource(Role(
+    "LambdaExecutionRole",
+    Path="/",
+    Policies=[Policy(
+        PolicyName="root",
+        PolicyDocument={
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Action": ["logs:*"],
+                "Resource": "arn:aws:logs:*:*:*",
+                "Effect": "Allow"
+            }]
+        })],
+    AssumeRolePolicyDocument={"Version": "2012-10-17", "Statement": [
+        {"Action": ["sts:AssumeRole"], "Effect": "Allow",
+         "Principal": {"Service": ["lambda.amazonaws.com"]}}]},
+))
+
+# Create the Lambda function
+foobar_function = t.add_resource(Function(
+    "FoobarFunction",
+    Code=Code(
+        ZipFile=Join("", code)
+    ),
+    Handler="index.handler",
+    Role=GetAtt("LambdaExecutionRole", "Arn"),
+    Runtime="nodejs",
+))
+
+# Create an API method for the lambda function
+# method = t.add_resource(Method(
+#     "FoobarMethod",
+#     RestApiId=Ref(rest_api),
+#     HttpMethod="GET",
+#     Integration=Integration(
+#         Type="LAMBDA",
+#         Uri=Ref(foobar_function)
+#     )
+# ))
+#
+# deployment = t.add_resource(Deployment(
+#     "TestDeployment",
+#     RestApiId=Ref(rest_api),
+#     StageName="Test"
+# ))
+
 
 print(t.to_json())
