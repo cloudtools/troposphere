df22b5ff8dbab63274ed79880d49ae71b49088d2
muikrad <muikrad@hotmail.com>
2016-04-19 18:53:30 -0400

    Remove extra import

--- troposphere/cloudformation.py
+++ troposphere/cloudformation.py
@@ -3,7 +3,7 @@
 #
 # See LICENSE file for full license.
 
-from . import AWSHelperFn, AWSObject, AWSProperty, Ref, BaseAWSObject
+from . import AWSHelperFn, AWSObject, AWSProperty, BaseAWSObject
 from .validators import integer, boolean, encoding
 
 
