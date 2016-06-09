0e8462f8c8f3c902f9f8e559504a8c6417716dc0
Yasser <yasir.rasheed@yahoo.com>
2016-06-09 17:26:42 +0200

    Update __init__.py

--- troposphere/__init__.py
+++ troposphere/__init__.py
@@ -26,7 +26,7 @@ AWS_REGION = 'AWS::Region'
 AWS_STACK_ID = 'AWS::StackId'
 AWS_STACK_NAME = 'AWS::StackName'
 
-valid_names = re.compile(r'^[a-zA-Z0-9]+$')
+valid_names = re.compile(r'^[a-zA-Z0-9\.\-\_]+$')
 
 
 class BaseAWSObject(object):
