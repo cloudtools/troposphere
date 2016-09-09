6c9e54f68d835bf26ad1d23b85ce9e856e905273
Michael Barrett <loki77@gmail.com>
2016-09-09 09:17:39 -0700

    Quiet down pyflakes

--- troposphere/dynamodb2.py
+++ troposphere/dynamodb2.py
@@ -1,6 +1,34 @@
-from .dynamodb import *  # NOQA
+from .dynamodb import (
+    AttributeDefinition,
+    KeySchema,
+    Key,
+    ProvisionedThroughput,
+    Projection,
+    GlobalSecondaryIndex,
+    LocalSecondaryIndex,
+    StreamSpecification,
+    Table,
+    attribute_type_validator,
+    key_type_validator,
+    projection_type_validator,
+)
 
 import warnings
+
+# Only way to make pyflakes shut up about unused imports
+assert AttributeDefinition
+assert KeySchema
+assert Key
+assert ProvisionedThroughput
+assert Projection
+assert GlobalSecondaryIndex
+assert LocalSecondaryIndex
+assert StreamSpecification
+assert Table
+assert attribute_type_validator
+assert key_type_validator
+assert projection_type_validator
+
 
 warnings.warn("This module has replaced troposphere.dynamodb. Please use that "
               "module instead, as troposphere.dynamodb2 will be being removed "
