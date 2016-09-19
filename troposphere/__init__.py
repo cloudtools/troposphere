bdd6c70732bba0fbc3fa7215709d7619e2897534
msirull <msirull@users.noreply.github.com>
2016-09-19 13:06:35 -0700

    Added cross-stack references
    Tested from the code I've been using for the last couple of weeks

--- troposphere/__init__.py
+++ troposphere/__init__.py
@@ -369,6 +369,12 @@ class Condition(AWSHelperFn):
     def JSONrepr(self):
         return self.data
 
+class ImportValue(AWSHelperFn):
+    def __init__(self, data):
+        self.data = {'Fn::ImportValue': data}
+
+    def JSONrepr(self):
+        return self.data
 
 class awsencode(json.JSONEncoder):
     def default(self, obj):
@@ -486,6 +492,7 @@ class Output(AWSDeclaration):
     props = {
         'Description': (basestring, False),
         'Value': (basestring, True),
+    	'Export': (dict, False),
     }
 
 
