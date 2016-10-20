bcda410641237856b1654d5a54b6d8631e4341fd
Richard Adams <Richard.Adams@Slayford.com>
2016-10-20 11:23:33 +0100

    Add support for S3 Lifecycle Rule move from singular NoncurrentVersionTransition to a set of NoncurrentVersionTransitions.  Using same methodology as the deprecation of Transition.

--- troposphere/s3.py
+++ troposphere/s3.py
@@ -106,6 +106,7 @@ class LifecycleRule(AWSProperty):
         'Id': (basestring, False),
         'NoncurrentVersionExpirationInDays': (positive_integer, False),
         'NoncurrentVersionTransition': (NoncurrentVersionTransition, False),
+        'NoncurrentVersionTransitions': ([NoncurrentVersionTransition], False),
         'Prefix': (basestring, False),
         'Status': (basestring, True),
         'Transition': (LifecycleRuleTransition, False),
@@ -125,6 +126,21 @@ class LifecycleRule(AWSProperty):
                     'Cannot specify both "Transition" and "Transitions" '
                     'properties on S3 Bucket Lifecycle Rule. Please use '
                     '"Transitions" since the former has been deprecated.')
+
+        if 'NoncurrentVersionTransition' in self.properties:
+            if 'NoncurrentVersionTransitions' not in self.properties:
+                # aws moved from a single transition to a list of them
+                # and deprecated 'NoncurrentVersionTransition', so let's
+                # just move it to the new property and not annoy the user.
+                self.properties['NoncurrentVersionTransitions'] = [
+                    self.properties.pop('NoncurrentVersionTransition')]
+            else:
+                raise ValueError(
+                    'Cannot specify both "NoncurrentVersionTransition" and '
+                    '"NoncurrentVersionTransitions" properties on S3 Bucket '
+                    'Lifecycle Rule. Please use '
+                    '"NoncurrentVersionTransitions" since the former has been '
+                    'deprecated.')
 
 
 class LifecycleConfiguration(AWSProperty):
