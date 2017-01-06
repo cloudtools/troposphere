6e385515cc792ea5048cc7edd009f1ad9e58dc54
Cian Brennan <cian.brennan@redbrick.dcu.ie>
2017-01-06 15:38:58 +0000

    Extra validation (#635)
    Added validation for parameters, and for reaching max resources on a template. Fixes #634

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,6 +1,7 @@
 import unittest
 
-from troposphere import Template
+from troposphere import Template, Parameter
+from troposphere.s3 import Bucket
 
 
 class TestInitArguments(unittest.TestCase):
@@ -21,6 +22,22 @@ class TestInitArguments(unittest.TestCase):
         value = 'foo'
         template = Template(Metadata=value)
         self.assertEqual(template.metadata, value)
+
+
+class TestValidate(unittest.TestCase):
+    def test_max_parameters(self):
+        template = Template()
+        for i in range(1, 61):
+            template.add_parameter(Parameter("parameter%d" % i, Type='String'))
+        with self.assertRaises(ValueError):
+            template.add_parameter(Parameter("Parameter61", Type='String'))
+
+    def test_max_resources(self):
+        template = Template()
+        for i in range(1, 201):
+            template.add_resource(Bucket(str(i)))
+        with self.assertRaises(ValueError):
+            template.add_resource(Bucket(str(201)))
 
 
 if __name__ == '__main__':
