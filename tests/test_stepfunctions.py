a1b38b4eeeb1f013f8756d2e152e2c4c6cc11798
syphon7 <syphon7@gmail.com>
2017-02-12 10:31:23 -0600

    Added step functions and basic tests (#661)

---
+++ tests/test_stepfunctions.py
@@ -0,0 +1,30 @@
+import unittest
+from troposphere.stepfunctions import Activity, StateMachine
+
+
+class TestStepFunctions(unittest.TestCase):
+    def test_activity(self):
+        activity = Activity(
+            'myactivity',
+            Name="testactivity",
+        )
+        self.assertEqual(activity.Name, 'testactivity')
+
+    def test_statemachine(self):
+        statemachine = StateMachine(
+            'mystatemachine',
+            DefinitionString="testdefinitionstring",
+            RoleArn="testinrolearn"
+        )
+        self.assertEqual(statemachine.RoleArn, 'testinrolearn')
+
+    def test_statemachine_missing_parameter(self):
+        StateMachine(
+            'mystatemachine',
+            DefinitionString="testdefinitionstring",
+        )
+        self.assertTrue(AttributeError)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ troposphere/stepfunctions.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2012-2013, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from . import AWSObject
+
+
+class Activity(AWSObject):
+    resource_type = "AWS::StepFunctions::Activity"
+    props = {
+        'Name': (basestring, True),
+    }
+
+
+class StateMachine(AWSObject):
+    resource_type = "AWS::StepFunctions::StateMachine"
+    props = {
+        'DefinitionString': (basestring, True),
+        'RoleArn': (basestring, True),
+
+    }
