776e61317d78c44bcc109b67edb793511cc37f40
Mark Peek <mark@peek.org>
2017-05-07 11:36:53 -0700

    Add MaximumExecutionFrequency to Config SourceDetails

---
+++ tests/test_config.py
@@ -0,0 +1,23 @@
+import unittest
+from troposphere.config import SourceDetails, ONE_HOUR
+
+
+class TestConfig(unittest.TestCase):
+    def test_SourceDetails(self):
+        SourceDetails(
+            EventSource="esource",
+            MaximumExecutionFrequency=ONE_HOUR,
+            MessageType="mtype",
+        ).to_dict()
+
+    def test_invalid_SourceDetails_MaximumExecutionFrequency(self):
+        with self.assertRaises(ValueError):
+            SourceDetails(
+                EventSource="esource",
+                MaximumExecutionFrequency="foo",
+                MessageType="mtype",
+            ).to_dict()
+
+
+if __name__ == '__main__':
+    unittest.main()
