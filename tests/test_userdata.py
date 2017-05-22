e572cef2f6205aa8f8cdd230967e158a54025e4c
Jae Kun Choi <JaeKun.Choi@sportsbet.com.au>
2017-05-23 00:07:00 +1000

    Added unit test for passing in Ref function

--- tests/test_userdata.py
+++ tests/test_userdata.py
@@ -56,6 +56,10 @@ class TestUserdata(unittest.TestCase):
     def test_nonexistant_file(self):
         self.assertRaises(IOError, self.create_result, 'nonexistant.sh')
 
+    def test_if_ref_is_replaced(self):
+        result = self.create_result('ref_func.sh')
+        answer = self.create_answer(['/opt/aws/bin/cfn-init -v --stack "Ref(\'AWS::StackName\')" \\\n'])
+        self.assertEqual(result, answer)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/userdata_test_scripts/ref_func.sh
@@ -0,0 +1,1 @@
+/opt/aws/bin/cfn-init -v --stack "Ref('AWS::StackName')" \
