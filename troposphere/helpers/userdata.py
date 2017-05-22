c0b8edbf9c7be0c855a607fa14b6a4bec477bc80
Jae Kun Choi <JaeKun.Choi@sportsbet.com.au>
2017-05-22 23:52:53 +1000

    Now Ref function is supported inside userdata

--- troposphere/helpers/userdata.py
+++ troposphere/helpers/userdata.py
@@ -6,6 +6,7 @@ from troposphere import Base64, Join
 def from_file(filepath, delimiter='', blanklines=False):
     """
     Imports userdata from a file.
+    Also supports passing Troposphere Ref function
 
     :type filepath: string
     :param filepath
@@ -23,16 +24,30 @@ def from_file(filepath, delimiter='', blanklines=False):
     :return The base64 representation of the file.
     """
 
+    ref_pattern = re.compile('(?P<prefix>.*)Ref\(\'(?P<ref_name>[a-zA-Z0-9\:]+)\'\)(?P<postfix>.*[^\\*])')
     data = []
 
     try:
         with open(filepath, 'r') as f:
             for line in f:
+                print line
                 if blanklines and line.strip('\n\r ') == '':
                     continue
 
-                data.append(line)
+                ref_ex = ref_pattern.match(line)
+
+                if ref_ex:
+                    data.append(ref_ex.group('prefix'))
+                    data.append(Ref(ref_ex.group('ref_name')))
+
+                    if ref_ex.group('postfix'):
+                        data.append(ref_ex.group('postfix'))
+                        continue
+
+                else:
+                    data.append(line)
     except IOError:
         raise IOError('Error opening or reading file: {}'.format(filepath))
 
+    print data
     return Base64(Join(delimiter, data))
