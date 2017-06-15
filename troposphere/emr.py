ae0fa90f463b1ce08a2b22b983d8613cb7584809
Brendan Murtagh <brendan.r.murtagh@gmail.com>
2017-06-15 15:58:37 -0400

    changed back to dict from basestring

--- troposphere/emr.py
+++ troposphere/emr.py
@@ -43,7 +43,7 @@ class SecurityConfiguration(AWSObject):
 
     props = {
         'Name': (basestring, False),
-        'SecurityConfiguration': (basestring, True)
+        'SecurityConfiguration': (dict, True)
     }
 
 
