e3c0c2c1db59c2aadc06b4e7c8b3f366aeda738a
Mark Peek <mark@peek.org>
2017-06-23 08:39:28 -0700

    Raise TypeError when a scaler AWSHelperFn is used in a list context

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -379,5 +379,26 @@ class TestJoin(unittest.TestCase):
             Join(10, "foobar")
 
 
+class TestHelperFn(unittest.TestCase):
+
+    def test_helperfn(self):
+        class FakeRef(AWSObject):
+            props = {
+            }
+
+        class Prop(AWSProperty):
+            props = {
+                'param': ([basestring], True),
+            }
+
+        fake = FakeRef('fakeref')
+        Prop(param=["fakeref"])
+        with self.assertRaises(TypeError):
+            Prop(param="fakeref")
+        Prop(param=[Ref(fake)])
+        with self.assertRaises(TypeError):
+            Prop(param=Ref(fake))
+
+
 if __name__ == '__main__':
     unittest.main()
