ce814f95b583b70bbef5046745d1daa25b78dd41
Jason Cragg <jcragg@gmail.com>
2017-08-23 11:01:13 -0400

    Add support for aurora-postgresql as a valid DB engine (#791)

--- troposphere/rds.py
+++ troposphere/rds.py
@@ -15,7 +15,7 @@ VALID_STORAGE_TYPES = ('standard', 'gp2', 'io1')
 VALID_DB_ENGINES = ('MySQL', 'mysql', 'oracle-se1', 'oracle-se2', 'oracle-se',
                     'oracle-ee', 'sqlserver-ee', 'sqlserver-se',
                     'sqlserver-ex', 'sqlserver-web', 'postgres', 'aurora',
-                    'mariadb')
+                    'aurora-postgresql', 'mariadb')
 VALID_LICENSE_MODELS = ('license-included', 'bring-your-own-license',
                         'general-public-license', 'postgresql-license')
 
