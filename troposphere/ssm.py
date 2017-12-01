3d9e82bc77acece19aa6a4ef0c02fbead4df9e66
Vladimir Simakhin <vladimir.simakhin@accolade.com>
2017-12-01 16:04:47 +0100

    fix the validator, otherwise it raises the error isinstance() arg 2 must be a type or tuple of types

--- troposphere/ssm.py
+++ troposphere/ssm.py
@@ -12,7 +12,7 @@ from .validators import (integer, boolean, s3_bucket_name, notification_type,
 class NotificationConfig(AWSProperty):
     props = {
         'NotificationArn': (basestring, False),
-        'NotificationEvents': ([notification_event], False),
+        'NotificationEvents': (notification_event, False),
         'NotificationType': (notification_type, False),
     }
 
