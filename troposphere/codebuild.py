3f8e3d2111f098decdfb16b92b6d07ab13600eac
Mark Peek <mark@peek.org>
2017-12-01 15:37:52 -0800

    Update AWS::CodeBuild::Project to 20171201 changes

--- troposphere/codebuild.py
+++ troposphere/codebuild.py
@@ -10,7 +10,7 @@ from .validators import integer, boolean
 class SourceAuth(AWSProperty):
     props = {
         'Resource': (basestring, False),
-        'Type': (basestring, False),
+        'Type': (basestring, True),
     }
 
     def validate(self):
@@ -56,8 +56,19 @@ class Artifacts(AWSProperty):
 class EnvironmentVariable(AWSProperty):
     props = {
         'Name': (basestring, True),
+        'Type': (basestring, False),
         'Value': (basestring, True),
     }
+
+    def validate(self):
+        valid_types = [
+            'PARAMETER_STORE',
+            'PLAINTEXT',
+        ]
+        env_type = self.properties.get('Type')
+        if env_type not in valid_types:
+            raise ValueError('EnvironmentVariable Type: must be one of %s' %
+                             ','.join(valid_types))
 
 
 class Environment(AWSProperty):
@@ -76,6 +87,23 @@ class Environment(AWSProperty):
         env_type = self.properties.get('Type')
         if env_type not in valid_types:
             raise ValueError('Environment Type: must be one of %s' %
+                             ','.join(valid_types))
+
+
+class ProjectCache(AWSProperty):
+    props = {
+        'Location': (basestring, False),
+        'Type': (basestring, True),
+    }
+
+    def validate(self):
+        valid_types = [
+            'NO_CACHE',
+            'S3',
+        ]
+        cache_type = self.properties.get('Type')
+        if cache_type not in valid_types:
+            raise ValueError('ProjectCache Type: must be one of %s' %
                              ','.join(valid_types))
 
 
@@ -113,11 +141,21 @@ class Source(AWSProperty):
                              "'GITHUB' Source Type.")
 
 
+class VpcConfig(AWSProperty):
+    props = {
+        'SecurityGroupIds': ([basestring], True),
+        'Subnets': ([basestring], True),
+        'VpcId': (basestring, True),
+    }
+
+
 class Project(AWSObject):
     resource_type = "AWS::CodeBuild::Project"
 
     props = {
         'Artifacts': (Artifacts, True),
+        'BadgeEnabled': (boolean, False),
+        'Cache': (ProjectCache, False),
         'Description': (basestring, False),
         'EncryptionKey': (basestring, False),
         'Environment': (Environment, True),
@@ -126,4 +164,5 @@ class Project(AWSObject):
         'Source': (Source, True),
         'Tags': (Tags, False),
         'TimeoutInMinutes': (integer, False),
+        'VpcConfig': (VpcConfig, False),
     }
