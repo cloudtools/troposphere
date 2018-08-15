eca23d2573b94e93b4db4c08f1538b4e22c7872a
slukes <sam_lukes@icloud.com>
2018-08-15 16:54:38 +0100

    Add get_or_add parameter method

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -236,6 +236,22 @@ class TestParameter(unittest.TestCase):
         with self.assertRaises(ValueError):
             t.to_json()
 
+    def test_get_or_add_adds(self):
+        t = Template()
+        p = Parameter("param", Type="String", Default="foo")
+        result = t.get_or_add_parameter(p)
+        self.assertEquals(t.parameters["param"], p)
+        self.assertEquals(result, p)
+
+    def test_add_or_get_returns_with_out_adding_duplicate(self):
+        t = Template()
+        p = Parameter("param", Type="String", Default="foo")
+        t.add_parameter(p)
+        result = t.get_or_add_parameter(p)
+        self.assertEquals(t.parameters["param"], p)
+        self.assertEquals(result, p)
+        self.assertEquals(len(t.parameters), 1)
+
     def test_property_default(self):
         p = Parameter("param", Type="String", Default="foo")
         p.validate()
