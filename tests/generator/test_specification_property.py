b979ab86c01e31be091db5ccab15c07223b004ed
DrLuke <Lukas@Jackowski.de>
2019-01-25 20:17:38 +0100

    Add unittests for Property

---
+++ tests/generator/test_specification_property.py
@@ -0,0 +1,109 @@
+import unittest
+from troposphere_gen.specification import Property
+from troposphere_gen.types import *
+
+
+class TestProperty(unittest.TestCase):
+    def test_primitive(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "PrimitiveType": "String",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(PrimitiveType, type(prop.primitive_type))
+        self.assertEqual(None, prop.type)
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+
+    def test_map_primitive(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "Type": "Map",
+            "PrimitiveItemType": "String",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(None, prop.primitive_type)
+        self.assertEqual(MapType, type(prop.type))
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+        self.assertEqual(PrimitiveType, type(prop.type.itemtype))
+
+    def test_map_subproperty(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "Type": "Map",
+            "ItemType": "SomeType",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(None, prop.primitive_type)
+        self.assertEqual(MapType, type(prop.type))
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+        self.assertEqual(Subproperty, type(prop.type.itemtype))
+
+    def test_list_primitive(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "Type": "List",
+            "PrimitiveItemType": "String",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(None, prop.primitive_type)
+        self.assertEqual(ListType, type(prop.type))
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+        self.assertEqual(PrimitiveType, type(prop.type.itemtype))
+
+    def test_list_subproperty(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "Type": "List",
+            "ItemType": "SomeType",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(None, prop.primitive_type)
+        self.assertEqual(ListType, type(prop.type))
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+        self.assertEqual(Subproperty, type(prop.type.itemtype))
+
+    def test_subproperty(self):
+        propertydict = {
+            "Documentation": "http://example.com/foo",
+            "Type": "SomeSubProperty",
+            "PrimitiveItemType": "String",
+            "Required": False,
+            "UpdateType": "Mutable"
+        }
+
+        prop = Property("TestProperty", propertydict)
+
+        self.assertEqual(None, prop.primitive_type)
+        self.assertEqual(Subproperty, type(prop.type))
+        self.assertEqual(None, prop.type.print_class)
+        self.assertEqual(None, prop.primitive_item_type)
+        self.assertEqual(None, prop.item_type)
+
+
+if __name__ == '__main__':
+    unittest.main()
