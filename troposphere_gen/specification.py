5e5b62c1e88735a7e06741b082cd64d01745d481
DrLuke <Lukas@Jackowski.de>
2019-01-25 22:10:45 +0100

    Refactor to reuse code

--- troposphere_gen/specification.py
+++ troposphere_gen/specification.py
@@ -8,59 +8,6 @@ from typing import Dict, List, Union
 from .types import PrimitiveType, Subproperty, MapType, ListType
 
 
-class Property():
-    """Parsed property"""
-
-    def __init__(self, name: str, propertydict: Dict) -> None:
-        self.name: str = name
-        self.documentation: str = None
-        self.duplicate_allowed: bool = None
-        self.primitive_type: PrimitiveType = None
-        self.required: bool = None
-        self.type: Union[Subproperty, ListType, MapType] = None
-        self._update_type: str = None
-
-        self.parse(propertydict)
-
-    def parse(self, propertydict: Dict) -> None:
-        """Parse JSON property definition"""
-        # Required fields for every property
-        self.documentation = propertydict["Documentation"]
-        self.update_type = propertydict["UpdateType"]
-        self.required = propertydict["Required"]
-
-        # Determine type
-        if "PrimitiveType" in propertydict:
-            self.primitive_type = PrimitiveType(propertydict["PrimitiveType"])
-        elif "Type" in propertydict:
-            if propertydict["Type"] == "List":
-                if "PrimitiveItemType" in propertydict:
-                    self.type = ListType(PrimitiveType(propertydict["PrimitiveItemType"]))
-                elif "ItemType" in propertydict:
-                    self.type = ListType(Subproperty(propertydict["ItemType"]))
-            elif propertydict["Type"] == "Map":
-                if "PrimitiveItemType" in propertydict:
-                    self.type = MapType(PrimitiveType(propertydict["PrimitiveItemType"]))
-                elif "ItemType" in propertydict:
-                    self.type = MapType(Subproperty(propertydict["ItemType"]))
-            else:
-                self.type = Subproperty(propertydict["Type"])
-
-        if "DuplicatesAllowed" in propertydict:
-            self.duplicate_allowed = propertydict["DuplicatesAllowed"]
-
-    @property
-    def update_type(self) -> str:
-        return self._update_type
-
-    @update_type.setter
-    def update_type(self, update_type: str) -> None:
-        if update_type in ["Immutable", "Mutable", "Conditional"]:
-            self._update_type = update_type
-        else:
-            raise ValueError("Invalid update type: %s" % update_type)
-
-
 class Attribute():
     """Parsed attribute"""
 
@@ -91,3 +38,39 @@ class Attribute():
                     self.type = MapType(Subproperty(attributedict["ItemType"]))
             else:
                 self.type = Subproperty(attributedict["Type"])
+
+
+class Property(Attribute):
+    """Parsed property"""
+
+    def __init__(self, name: str, propertydict: Dict) -> None:
+        super(Property, self).__init__(name, propertydict)
+        self.documentation: str = None
+        self.duplicate_allowed: bool = None
+        self.required: bool = None
+        self._update_type: str = None
+
+        self.parse(propertydict)
+
+    def parse(self, propertydict: Dict) -> None:
+        """Parse JSON property definition"""
+        super(Property, self).parse(propertydict)
+
+        # Required fields for every property
+        self.documentation = propertydict["Documentation"]
+        self.update_type = propertydict["UpdateType"]
+        self.required = propertydict["Required"]
+
+        if "DuplicatesAllowed" in propertydict:
+            self.duplicate_allowed = propertydict["DuplicatesAllowed"]
+
+    @property
+    def update_type(self) -> str:
+        return self._update_type
+
+    @update_type.setter
+    def update_type(self, update_type: str) -> None:
+        if update_type in ["Immutable", "Mutable", "Conditional"]:
+            self._update_type = update_type
+        else:
+            raise ValueError("Invalid update type: %s" % update_type)
