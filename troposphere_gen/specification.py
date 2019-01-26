182c6c5fd0f43ed1b8b979c7a9fc91a80cd34804
DrLuke <Lukas@Jackowski.de>
2019-01-26 12:09:38 +0100

    Accomodate for subproperties

--- troposphere_gen/specification.py
+++ troposphere_gen/specification.py
@@ -44,22 +44,28 @@ class Property(Attribute):
     """Parsed property"""
 
     def __init__(self, name: str, propertydict: Dict) -> None:
-        super(Property, self).__init__(name, propertydict)
         self.documentation: str = None
         self.duplicate_allowed: bool = None
         self.required: bool = None
         self._update_type: str = None
+        self.properties: Dict[str, Property] = {}
 
-        self.parse(propertydict)
+        super(Property, self).__init__(name, propertydict)
 
     def parse(self, propertydict: Dict) -> None:
         """Parse JSON property definition"""
-        super(Property, self).parse(propertydict)
-
-        # Required fields for every property
         self.documentation = propertydict["Documentation"]
+
+        # If property contains subproperties, only parse those
+        if "Properties" in propertydict:
+            for name, subpropertydict in propertydict["Properties"].items():
+                self.properties[name] = Property(name, subpropertydict)
+            return
+
         self.update_type = propertydict["UpdateType"]
         self.required = propertydict["Required"]
+
+        super(Property, self).parse(propertydict)
 
         if "DuplicatesAllowed" in propertydict:
             self.duplicate_allowed = propertydict["DuplicatesAllowed"]
