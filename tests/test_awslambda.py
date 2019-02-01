d59c0bfd7dad99091e79a22fb3f9adb5074adae6
cedricATBQ <cedric.rumiano@atbq.fr>
2019-02-01 11:50:27 +0100

    Fix for AWS Lambda reserved environment variables

--- tests/test_awslambda.py
+++ tests/test_awslambda.py
@@ -73,6 +73,23 @@ class TestAWSLambda(unittest.TestCase):
             self.assertTrue('Invalid environment variable name: %s' % var
                             in context.exception)
 
+    def test_environment_variable_reserved(self):
+        for var in ['AWS_ACCESS_KEY', 'AWS_ACCESS_KEY_ID',
+                    'AWS_LAMBDA_FUNCTION_MEMORY_SIZE']:
+            with self.assertRaises(ValueError) as context:
+                Environment(Variables={var: 'value'})
+
+            self.assertTrue("Lambda Function environment variables names "
+                            "can't be none of"
+                            in context.exception.args[0])
+
+    def test_environment_variable_not_reserved(self):
+        for var in ['NODE_PATH', 'NODE_ENV', 'FOO']:
+            try:
+                Environment(Variables={var: 'value'})
+            except ValueError:
+                self.fail("Environment() raised ValueError")
+
 
 if __name__ == '__main__':
     unittest.main()
