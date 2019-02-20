ddbf5288a07264fd1eb1beac4a41b4f223e5eecb
Michael <michael-k@users.noreply.github.com>
2019-02-20 15:21:38 +0000

    Discourage usage of Python 3.4 (#1326)
    * Discourage usage of Python 3.4, but do not prevent it yet
       Python 3.4 will reach its EOL in March 2019, see
       https://www.python.org/dev/peps/pep-0429/#release-schedule

--- setup.py
+++ setup.py
@@ -69,7 +69,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 2.7",
     ],
 
@@ -83,6 +82,7 @@ setup(
         'scripts/cfn2py'
     ],
 
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=file_contents("requirements.txt"),
     test_suite="tests",
     tests_require=["awacs>=0.8"],
