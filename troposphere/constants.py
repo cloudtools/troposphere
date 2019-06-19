837f6ba3e1662952ef29d5b33760d9cfa210a79b
Lou Ferrand <lou.ferrand@ekino.com>
2019-06-19 13:53:00 +0200

    Add resource for Amazon Managed Streaming for Apache Kafka (Amazon MSK)

--- troposphere/constants.py
+++ troposphere/constants.py
@@ -537,6 +537,17 @@ ELASTICSEARCH_I2_XLARGE = 'i2.xlarge.elasticsearch'
 ELASTICSEARCH_I2_2XLARGE = 'i2.2xlarge.elasticsearch'
 
 #
+# Kafka brokers instance types
+#
+
+KAFKA_M5_LARGE = 'kafka.m5.large'
+KAFKA_M5_XLARGE = 'kafka.m5.xlarge'
+KAFKA_M5_2XLARGE = 'kafka.m5.2xlarge'
+KAFKA_M5_3XLARGE = 'kafka.m5.4xlarge'
+KAFKA_M5_12XLARGE = 'kafka.m5.12xlarge'
+KAFKA_M5_24XLARGE = 'kafka.m5.24xlarge'
+
+#
 # Parameter types
 #
 
