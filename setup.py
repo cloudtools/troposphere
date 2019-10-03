6c45793dad6500ad3cc329f185560536f62d82bc
asafc64 <asafc64@gmail.com>
2019-10-03 11:27:40 +0300

    Increment version to 0.0.30

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='ionosphere',
-    version="0.0.29",
+    version="0.0.30",
     description="Azure Resource Manager Template creation library",
     long_description=readme(),
     author="Alex Azarh",
