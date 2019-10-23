3c79108f9d7b975e360d75b3f43a7cf5a1a50ac4
Michael <michael-k@users.noreply.github.com>
2019-10-23 13:55:28 +0000

    Make Python 3.8 support official (#1513)

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         "Operating System :: POSIX :: Linux",
 
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.5",
