ff1d9b41263e0c5c2f4713b02206ad05afe64f43
Grant Soyka <gsoyka@gmail.com>
2020-04-08 15:20:39 -0600

    Add athena workgroup and associated resources

--- troposphere/athena.py
+++ troposphere/athena.py
@@ -3,7 +3,7 @@
 #
 # See LICENSE file for full license.
 
-from . import AWSObject
+from . import AWSObject, AWSProperty, Tags
 
 
 class NamedQuery(AWSObject):
@@ -14,4 +14,61 @@ class NamedQuery(AWSObject):
         'Description': (basestring, False),
         'Name': (basestring, False),
         'QueryString': (basestring, True),
+    }
+
+
+class EncryptionConfiguration(AWSProperty):
+    props = {
+        'EncryptionOption': (basestring, True),
+        'KmsKey': (basestring, False)
+    }
+
+
+class ResultConfiguration(AWSProperty):
+    props = {
+        'EncryptionConfiguration': (EncryptionConfiguration, False),
+        'OutputLocation': (basestring, False)
+    }
+
+
+class ResultConfigurationUpdates(AWSProperty):
+    props = {
+        'EncryptionConfiguration': (EncryptionConfiguration, False),
+        'OutputLocation': (basestring, False),
+        'RemoveEncryptionConfiguration': (bool, False),
+        'RemoveOutputLocation': (bool, False)
+    }
+
+
+class WorkGroupConfiguration(AWSProperty):
+    props = {
+        'BytesScannedCutoffPerQuery': (int, False),
+        'EnforceWorkGroupConfiguration': (bool, False),
+        'PublishCloudWatchMetricsEnabled': (bool, False),
+        'RequesterPaysEnabled': (bool, False),
+        'ResultConfiguration': (ResultConfiguration, False)
+    }
+
+
+class WorkGroupConfigurationUpdates(AWSProperty):
+    props = {
+        'BytesScannedCutoffPerQuery': (int, False),
+        'EnforceWorkGroupConfiguration': (bool, False),
+        'PublishCloudWatchMetricsEnabled': (bool, False),
+        'RequesterPaysEnabled': (bool, False),
+        'ResultConfigurationUpdates': (ResultConfigurationUpdates, False)
+    }
+
+
+class WorkGroup(AWSObject):
+    resource_type = "AWS::Athena::WorkGroup"
+
+    props = {
+        'Description': (basestring, False),
+        'Name': (basestring, True),
+        'RecursiveDeleteOption': (bool, False),
+        'State': (basestring, False),
+        'Tags': (Tags, False),
+        'WorkGroupConfiguration': (WorkGroupConfiguration, False),
+        'WorkGroupConfigurationUpdates': (WorkGroupConfigurationUpdates, False)
     }
