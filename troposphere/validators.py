80eb3c11eb9ef93cf5156f862c76f41ecee71b0e
tnielsen2 <trentnielsen84@gmail.com>
2020-08-21 15:00:47 -0400

    Remove return status

--- troposphere/validators.py
+++ troposphere/validators.py
@@ -681,10 +681,9 @@ def findingsfilter_action(action):
     return action
 
 def ecs_efs_encryption_status(status):
-    allowed_values = ['ENABLED', 'DISABLED']
+    allowed_values = ["ENABLED", "DISABLED"]
     if status not in allowed_values:
         raise ValueError(
             f'ECS EFS Encryption in transit can only be one of',
             allowed_values
         )
-    return status
