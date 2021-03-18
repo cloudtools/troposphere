9e177474ab5d2d7447f76f70d2722963fe672cba
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2021-03-18 15:30:55 +0500

    update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'cloudtools'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.4.1'
+release = '2.6.4'
 
 
 # -- General configuration ---------------------------------------------------
