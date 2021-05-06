153e8b1c38d5524bc21f20d34fe52d23f04dfbbc
Noam Tamim <noam.tamim@kaltura.com>
2021-05-06 14:36:29 +0300

    Move docstring to the correct place

--- troposphere/helpers/userdata.py
+++ troposphere/helpers/userdata.py
@@ -36,16 +36,17 @@ def from_file(filepath, delimiter="", blanklines=False):
     return Base64(Join(delimiter, data))
 
 
-"""Imports userdata from a file, using Sub for replacing inline variables such as ${AWS::Region}
-
-:type filepath: string
-
-:param filepath  The absolute path to the file.
-
-rtype: troposphere.Base64
-:return The base64 representation of the file.
-"""
 def from_file_sub(filepath):
+    """Imports userdata from a file, using Sub for replacing inline variables such as ${AWS::Region}
+
+    :type filepath: string
+
+    :param filepath  The absolute path to the file.
+
+    rtype: troposphere.Base64
+    :return The base64 representation of the file.
+    """
+    
     try:
         with open(filepath, "rt") as f:
             data = f.read()
