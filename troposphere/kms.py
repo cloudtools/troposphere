979b521a037b44b300e02d66fa0dbd967e078575
Mark Peek <mark@peek.org>
2021-07-04 12:47:22 -0700

    Update KMS per 2021-06-17 changes

--- troposphere/kms.py
+++ troposphere/kms.py
@@ -5,7 +5,7 @@
 
 from . import AWSObject, Tags
 from .compat import policytypes
-from .validators import boolean, integer_range, key_usage_type
+from .validators import boolean, integer, integer_range, key_usage_type
 
 
 class Alias(AWSObject):
@@ -26,4 +26,17 @@ class Key(AWSObject):
         "KeyUsage": (key_usage_type, False),
         "PendingWindowInDays": (integer_range(7, 30), False),
         "Tags": ((Tags, list), False),
+    }
+
+
+class ReplicaKey(AWSObject):
+    resource_type = "AWS::KMS::ReplicaKey"
+
+    props = {
+        "Description": (str, False),
+        "Enabled": (boolean, False),
+        "KeyPolicy": (dict, True),
+        "PendingWindowInDays": (integer, False),
+        "PrimaryKeyArn": (str, True),
+        "Tags": (Tags, False),
     }
