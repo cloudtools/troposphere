b60ad1f140b2549be7958569a6829ab605480051
Mark Peek <mark@peek.org>
2021-12-27 12:57:42 -0800

    Auto-generate CodeDeploy

---
+++ scripts/patches/codedeploy.py
@@ -0,0 +1,68 @@
+patches = [
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.EC2TagFilter",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.Ec2TagFilters",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::CodeDeploy::DeploymentGroup/Properties/Ec2TagFilters/ItemType",
+        "value": "Ec2TagFilters",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.EC2TagSetListObject/Properties/Ec2TagGroup/ItemType",
+        "value": "Ec2TagFilters",
+    },
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.EC2TagSet",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.Ec2TagSet",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::CodeDeploy::DeploymentGroup/Properties/Ec2TagSet/Type",
+        "value": "Ec2TagSet",
+    },
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.EC2TagSetListObject",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.Ec2TagSetListObject",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.Ec2TagSet/Properties/Ec2TagSetList/ItemType",
+        "value": "Ec2TagSetListObject",
+    },
+    # backward compatibility
+    {
+        "op": "copy",
+        "from": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.TagFilter",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.OnPremisesInstanceTagFilters",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::CodeDeploy::DeploymentGroup/Properties/OnPremisesInstanceTagFilters/ItemType",
+        "value": "OnPremisesInstanceTagFilters",
+    },
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.ELBInfo",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.ElbInfoList",
+    },
+    # backward compatibility
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodeDeploy::DeploymentGroup.LoadBalancerInfo/Properties/ElbInfoList/ItemType",
+        "value": "ElbInfoList",
+    },
+]
---
+++ troposphere/validators/codedeploy.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2012-2021, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+from . import exactly_one, mutually_exclusive
+
+KEY_ONLY = "KEY_ONLY"
+VALUE_ONLY = "VALUE_ONLY"
+KEY_AND_VALUE = "KEY_AND_VALUE"
+
+
+def deployment_option_validator(x):
+    """
+    Property: DeploymentStyle.DeploymentOption
+    """
+    valid_values = ["WITH_TRAFFIC_CONTROL", "WITHOUT_TRAFFIC_CONTROL"]
+    if x not in valid_values:
+        raise ValueError(
+            "Deployment Option value must be one of: %s" % ", ".join(valid_values)
+        )
+    return x
+
+
+def deployment_type_validator(x):
+    """
+    Property: DeploymentStyle.DeploymentType
+    """
+    valid_values = ["IN_PLACE", "BLUE_GREEN"]
+    if x not in valid_values:
+        raise ValueError(
+            "Deployment Type value must be one of: %s" % ", ".join(valid_values)
+        )
+    return x
+
+
+def validate_load_balancer_info(self):
+    """
+    Class: LoadBalancerInfo
+    """
+    conds = ["ElbInfoList", "TargetGroupInfoList"]
+    exactly_one(self.__class__.__name__, self.properties, conds)
+
+
+def validate_deployment_group(self):
+    """
+    Class: DeploymentGroup
+    """
+    ec2_conds = ["EC2TagFilters", "Ec2TagSet"]
+    onPremises_conds = ["OnPremisesInstanceTagFilters", "OnPremisesTagSet"]
+    mutually_exclusive(self.__class__.__name__, self.properties, ec2_conds)
+    mutually_exclusive(self.__class__.__name__, self.properties, onPremises_conds)
