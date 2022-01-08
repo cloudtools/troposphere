bf6e4f1cc3e71e0285309efe8253b884ffba77e9
Mark Peek <mark@peek.org>
2022-01-08 09:32:41 -0800

    Auto-generate SNS

---
+++ scripts/patches/sns.py
@@ -0,0 +1,8 @@
+patches = [
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/ResourceTypes/AWS::SNS::Subscription",
+        "path": "/ResourceTypes/AWS::SNS::SubscriptionResource",
+    },
+]
---
+++ troposphere/validators/sns.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2012-2022, Mark Peek <mark@peek.org>
+# All rights reserved.
+#
+# See LICENSE file for full license.
+
+
+from ..compat import validate_policytype
+
+
+def policytypes(policy):
+    """
+    Property: TopicPolicy.PolicyDocument
+    """
+    return validate_policytype(policy)
