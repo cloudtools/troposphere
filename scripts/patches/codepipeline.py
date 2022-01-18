0d30fba3c0229cccc2a7ad96374974f30a2dd1f6
Mark Peek <mark@peek.org>
2022-01-17 16:22:27 -0800

    Auto-generate CodePipeline

---
+++ scripts/patches/codepipeline.py
@@ -0,0 +1,63 @@
+patches = [
+    # backward compatibility
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.StageTransition",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.DisableInboundStageTransitions",
+    },
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::CodePipeline::Pipeline/Properties/DisableInboundStageTransitions/ItemType",
+        "value": "DisableInboundStageTransitions",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.StageDeclaration",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.Stages",
+    },
+    {
+        "op": "replace",
+        "path": "/ResourceTypes/AWS::CodePipeline::Pipeline/Properties/Stages/ItemType",
+        "value": "Stages",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.InputArtifact",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.InputArtifacts",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.ActionDeclaration/Properties/InputArtifacts/ItemType",
+        "value": "InputArtifacts",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.OutputArtifact",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.OutputArtifacts",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.ActionDeclaration/Properties/OutputArtifacts/ItemType",
+        "value": "OutputArtifacts",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.ActionDeclaration",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.Actions",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.Stages/Properties/Actions/ItemType",
+        "value": "Actions",
+    },
+    {
+        "op": "move",
+        "from": "/PropertyTypes/AWS::CodePipeline::Pipeline.BlockerDeclaration",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.Blockers",
+    },
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::CodePipeline::Pipeline.Stages/Properties/Blockers/ItemType",
+        "value": "Blockers",
+    },
+]
