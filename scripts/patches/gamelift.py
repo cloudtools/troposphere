68ef3b7f63f4441d95aeea6e7fc3e56db3bafdf1
Mark Peek <mark@peek.org>
2022-12-06 07:02:45 -0800

    Patch GameLift::Fleet to add type for AnywhereConfiguration

---
+++ scripts/patches/gamelift.py
@@ -0,0 +1,7 @@
+patches = [
+    {
+        "op": "add",
+        "path": "/ResourceTypes/AWS::GameLift::Fleet/Properties/AnywhereConfiguration/Type",
+        "value": "AnywhereConfiguration",
+    },
+]
