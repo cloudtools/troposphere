574d2ebf2928052d13e9b8d9a279e09ca9b69650
Mark Peek <mark@peek.org>
2023-04-09 10:47:14 -0700

    Fix Macie jsonpatch for spec 119.0.0 changes

--- scripts/patches/macie.py
+++ scripts/patches/macie.py
@@ -4,9 +4,4 @@ patches = [
         "path": "/PropertyTypes/AWS::Macie::FindingsFilter.FindingCriteria/Properties/Criterion/Type",
         "value": "Map",
     },
-    # Remove FindingsFilterListItem attribute property
-    {
-        "op": "remove",
-        "path": "/PropertyTypes/AWS::Macie::FindingsFilter.FindingsFilterListItem",
-    },
 ]
