aae0832d586322ad93856256f5e69ec7e2da5a34
M. Crichton <email@crichton.ma>
2023-04-25 11:25:38 +0100

    Support "elastic" throughput mode for EFS
    Add the missing "elastic" type to EFS throughput mode validation.
    
    See: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-efs-filesystem.html#cfn-efs-filesystem-throughputmode

--- troposphere/validators/efs.py
+++ troposphere/validators/efs.py
@@ -6,6 +6,7 @@
 from . import one_of
 
 Bursting = "bursting"
+Elastic = "elastic"
 Provisioned = "provisioned"
 
 
@@ -13,7 +14,7 @@ def throughput_mode_validator(mode):
     """
     Property: FileSystem.ThroughputMode
     """
-    valid_modes = [Bursting, Provisioned]
+    valid_modes = [Bursting, Elastic, Provisioned]
     if mode not in valid_modes:
         raise ValueError(
             'ThroughputMode must be one of: "%s"' % (", ".join(valid_modes))
