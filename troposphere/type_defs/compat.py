14a8b356e843bb4eb6eab1385173206ad83f5585
Michael K <michael-k@users.noreply.github.com>
2023-10-21 21:07:11 +0100

    Drop support for Python 3.7 (#2190)
    Python 3.7 reached its EOL on 2023-06-27, see
    https://peps.python.org/pep-0537/#lifespan
    
    Python 3.7 was removed from the test matrix in commit
    bf8730b9a4307ee3e75f05d95c2ccdc23078a6ba.

--- troposphere/type_defs/compat.py
+++ troposphere/type_defs/compat.py
@@ -2,9 +2,4 @@
 # flake8: noqa
 from __future__ import annotations
 
-import sys
-
-if sys.version_info < (3, 8):  # 3.7
-    from typing_extensions import Final, Literal, Protocol, SupportsIndex
-else:
-    from typing import Final, Literal, Protocol, SupportsIndex
+from typing import Final, Literal, Protocol, SupportsIndex
