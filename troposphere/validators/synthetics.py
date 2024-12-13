78d14e0215557fa75c5ac9a7e4854af0b9018524
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2024-12-13 17:46:30 +0400

    Update list of valid Synthetics::Canary runtime versions

--- troposphere/validators/synthetics.py
+++ troposphere/validators/synthetics.py
@@ -10,13 +10,27 @@ def canary_runtime_version(runtime_version):
     """
 
     valid_runtime_versions = [
+        "syn-nodejs-playwright-1.0",
         "syn-nodejs-puppeteer-4.0",
         "syn-nodejs-puppeteer-5.0",
         "syn-nodejs-puppeteer-5.1",
+        "syn-nodejs-puppeteer-5.2",
         "syn-nodejs-puppeteer-6.0",
         "syn-nodejs-puppeteer-6.1",
         "syn-nodejs-puppeteer-6.2",
         "syn-nodejs-puppeteer-7.0",
+        "syn-nodejs-puppeteer-8.0",
+        "syn-nodejs-puppeteer-9.0",
+        "syn-nodejs-puppeteer-9.1",
+        "syn-python-selenium-1.0",
+        "syn-python-selenium-1.1",
+        "syn-python-selenium-1.2",
+        "syn-python-selenium-1.3",
+        "syn-python-selenium-2.0",
+        "syn-python-selenium-2.1",
+        "syn-python-selenium-3.0",
+        "syn-python-selenium-4.0",
+        "syn-python-selenium-4.1",
     ]
     if runtime_version not in valid_runtime_versions:
         raise ValueError(
