c915c77d3b03aaf6a62abdef692c02decec5a83c
Mark Peek <mark@peek.org>
2025-01-14 15:01:34 -0800

    Tweak GameLift patches

--- scripts/patches/gamelift.py
+++ scripts/patches/gamelift.py
@@ -4,4 +4,14 @@ patches = [
         "path": "/ResourceTypes/AWS::GameLift::Fleet/Properties/AnywhereConfiguration/Type",
         "value": "AnywhereConfiguration",
     },
+    {
+        # Prefer ContainerFleet.LocationConfiguration over GameLift::Fleet.LocationConfiguration
+        "op": "remove",
+        "path": "/PropertyTypes/AWS::GameLift::Fleet.LocationConfiguration",
+    },
+    {
+        # Prefer GameLift::ContainerFleet.ScalingPolicy over GameLift::Fleet.ScalingPolicy
+        "op": "remove",
+        "path": "/PropertyTypes/AWS::GameLift::ContainerFleet.ScalingPolicy",
+    },
 ]
