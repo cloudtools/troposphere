4397e8339310ab36b5d31cecdeafc39a392d5dab
wuiscmc <971584+wuiscmc@users.noreply.github.com>
2025-02-14 13:09:11 +0000

    Updates from spec version 191.0.0

--- troposphere/transfer.py
+++ troposphere/transfer.py
@@ -145,15 +145,13 @@ class EndpointDetails(AWSProperty):
 
 class IdentityProviderDetails(AWSProperty):
     """
-    `IdentityProviderDetails <http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-transfer-server-identityproviderdetails.html>`__
+    `IdentityProviderDetails <http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-transfer-webapp-identityproviderdetails.html>`__
     """
 
     props: PropsDictType = {
-        "DirectoryId": (str, False),
-        "Function": (str, False),
-        "InvocationRole": (str, False),
-        "SftpAuthenticationMethods": (str, False),
-        "Url": (str, False),
+        "ApplicationArn": (str, False),
+        "InstanceArn": (str, False),
+        "Role": (str, False),
     }
 
 
