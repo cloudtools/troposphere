f82ad24817bc0d7fabdfe9e0da3acc4a4d44214b
Carlos Mateos <wuiscmc@gmail.com>
2025-02-17 00:19:25 +0100

    version 4.11.0

--- troposphere/__init__.py
+++ troposphere/__init__.py
@@ -41,7 +41,7 @@ if TYPE_CHECKING:
     else:
         from typing import Final
 
-__version__ = "4.9.1"
+__version__ = "4.11.0"
 
 # constants for DeletionPolicy and UpdateReplacePolicy
 Delete: Final = "Delete"
