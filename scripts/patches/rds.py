9db781bcef4741455fdbb193b82e1ba6b824c3eb
Mark Peek <mark@peek.org>
2025-04-08 13:59:59 -0700

    Patch RDS to being back some missing properties
    Spec 193.0.0 removed these properties but it wasn't clear
    that was correct as the property definitions remained. This
    commit brings them back (for now):
    
    Properties added back:
        AWS::RDS::DBInstance.CertificateDetails
        AWS::RDS::DBInstance.Endpoint
        AWS::RDS::GlobalCluster.GlobalEndpoint

--- scripts/patches/rds.py
+++ scripts/patches/rds.py
@@ -11,4 +11,29 @@ patches = [
         "path": "/ResourceTypes/AWS::RDS::DBSubnetGroup/Properties/SubnetIds/PrimitiveType",
         "value": "list",
     },
+    # Spec 193.0.0 removed these properties, so add them back in
+    {
+        "op": "add",
+        "path": "/ResourceTypes/AWS::RDS::DBInstance/Properties/CertificateDetails",
+        "value": {
+            "Type": "CertificateDetails",
+            "Required": False,
+        },
+    },
+    {
+        "op": "add",
+        "path": "/ResourceTypes/AWS::RDS::DBInstance/Properties/Endpoint",
+        "value": {
+            "Type": "Endpoint",
+            "Required": False,
+        },
+    },
+    {
+        "op": "add",
+        "path": "/ResourceTypes/AWS::RDS::GlobalCluster/Properties/GlobalEndpoint",
+        "value": {
+            "Type": "GlobalEndpoint",
+            "Required": False,
+        },
+    },
 ]
