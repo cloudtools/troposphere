dcc49f5d8cd80693bae3ff3f64b57c86af875286
Mark Peek <mark@peek.org>
2025-12-07 16:02:44 -0800

    Add AWS::ARCRegionSwitch

---
+++ scripts/patches/arcregionswitch.py
@@ -0,0 +1,7 @@
+patches = [
+    {
+        "op": "replace",
+        "path": "/PropertyTypes/AWS::ARCRegionSwitch::Plan.ParallelExecutionBlockConfiguration/Properties/Steps/ItemType",
+        "value": "object",
+    },
+]
